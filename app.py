a2dd641004379c34c27173c699664d31f0ad5f7d
Charles Hamilton <crhamiltonj@gmail.com>
2019-02-28 14:26:37 -0500

    Removed call to create_tables from app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.secret_key = 'AAABBBCCCDDD'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)
